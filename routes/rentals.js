eef6d1a37eeac9d387dbfb93fbe28f34c3b2c8ec
tavolop3 <tavoellopez@gmail.com>
2022-07-24 19:14:29 -0300

    Fixed bug tested

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,6 +1,7 @@
 const {Rental, validate} = require('../models/rental');
 const {Customer} = require('../models/customer');
 const {Movie} = require('../models/movie');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
