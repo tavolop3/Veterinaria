8a35c8dfd132708c569a0535328df5c3edc49a50
tavolop3 <tavo.lop33@gmail.com>
2023-02-21 23:41:45 -0300

    Seting up test db and added logging in console winston

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function(){
+    const db = config.get('db');
     mongoose.set('strictQuery', false);
-    mongoose.connect('mongodb://localhost/vidly')
-        .then(() => winston.info('Succesfully connected to the database.'));
+    mongoose.connect(db)
+        .then(() => winston.info(`Succesfully connected to ${db}...`));
 }
--- startup/logging.js
+++ startup/logging.js
@@ -6,6 +6,8 @@ module.exports = function() {
     winston.exceptions.handle(
         new winston.transports.Console({ colorize: true, prettyPrint: true }),
         new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+
+    winston.add(new winston.transports.Console({ level: 'info', colorize: true, prettyPrint: true }));
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));
     winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly',
                                                  level: 'info', options: { useUnifiedTopology: true } }));
