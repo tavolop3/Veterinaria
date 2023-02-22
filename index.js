3afc61ce17592a2d1e10eae9460c392d02037ebf
tavolop3 <tavo.lop33@gmail.com>
2023-02-22 00:06:50 -0300

    Setup supertest and first db test

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     winston.info(`Listening on port ${port}...`);
 });
+
+module.exports = server;
