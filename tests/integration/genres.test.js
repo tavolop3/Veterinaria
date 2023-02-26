e9cb17a0dd2866426d6f09e965f4065170ee0822
tavolop3 <tavo.lop33@gmail.com>
2023-02-25 23:39:58 -0300

    Added genres POST tests

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 let server;
 const {Genre} = require('../../models/genre');
+const {User} = require('../../models/user');
 const mongoose = require('mongoose');
 
 describe('/api/genres', () => {
@@ -44,4 +45,58 @@ describe('/api/genres', () => {
         });
     });
 
+    describe('POST /', () => {
+        let token;
+        let name; 
+
+        const exec = async() => {
+            return await request(server)
+                .post('/api/genres')
+                .set('x-auth-token', token)
+                .send({ name });
+        }
+
+        beforeEach(() => {
+            token = new User().generateAuthToken()
+            name = 'genre1';
+        })
+
+        it('should return 401 if client is not logged in', async() => {
+            token = '';
+
+            const res = await exec();
+                
+            expect(res.status).toBe(401);
+        });
+
+        it('should return 400 if genre is less than 3 characters', async() => {
+            name = '12';
+
+            const res = await exec();
+            
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if genre is greater than 15 characters', async() => {
+            name = new Array(17).join('a');
+
+            const res = await exec();
+            
+            expect(res.status).toBe(400);
+        });
+
+        it('should save the genre if it is valid', async() => {
+            await exec();
+            
+            const genre = await Genre.find({ name: 'genre1' });
+
+            expect(genre).not.toBeNull();
+        });
+
+        it('should have the genre if valid', async() => {
+            const res = await exec();
+
+            expect(res.body).toHaveProperty('name', 'genre1');
+        });
+    });
 });
