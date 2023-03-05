44928b086b995ff917a9adab82d0341ef946edf8
tavolop3 <tavo.lop33@gmail.com>
2023-03-05 12:07:20 -0300

    Preparing for prod

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/db')();
 require('./startup/logging')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
