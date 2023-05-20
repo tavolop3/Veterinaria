92e8dafcf45436315bb8d4069d51a220167b63c5
Matias Geringer <matiasgeringer7@gmail.com>
2023-05-20 10:40:12 -0300

    Implementa la funcionalidad de cerrar sesion

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -47,31 +47,4 @@ router.get('/yo', isAuthenticated, async(req,res) => {
     // res.send(user);
 })
 
-/*  el metodo permite que un usuari
-    realize la modificacion
-    de su mail o contraseña
-*/
-.post('/modificar-datos', async (req, res) => {
-  const { mail1, mail2, contraseña1, contraseña2 } = req.body;
-  let user = await User.findOne({ mail: mail1 });
-  if (!user) return res.status(400).send('El mail ingresado no pertenece a ningun usuario en el sistema');
-  if (user.contraseña != contraseña1) return res.status(400).send('La contraseña ingresada no es correcta');
-  if (mail2 === "") mail2 = mail1;
-  if (contraseña2 === "") contraseña2 = contraseña1;
-  let updatedFields = {};
-  if (mail2 !== "") updatedFields.mail = mail2;
-  if (contraseña2 !== "") updatedFields.contraseña = contraseña2;
-  try {
-    await User.updateOne({ mail: mail1 }, { $set: updatedFields });
-    return res.redirect('/indexCliente');
-  } catch (error) {
-    return res.json({
-      resultado: false,
-      msg: 'El usuario no se pudo modificar',
-      error
-    });
-  }
-})
-
 module.exports = router;
-
