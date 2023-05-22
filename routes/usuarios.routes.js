af81a731f9b35da1ddfcd8c54ea0cdcd26243c0c
Matias Geringer <matiasgeringer7@gmail.com>
2023-05-22 17:48:50 -0300

    Anexo entre la modificacion y el index

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const autenticado = require('../middleware/autenticado');
 const { default: mongoose } = require('mongoose');
-const {User} = require('../models/user');
+const {User, encriptarContraseña} = require('../models/user');
 const bcrypt = require('bcrypt');
 
 // Para ver el usuario actual
@@ -20,22 +20,26 @@ router.get('/yo', autenticado, async(req,res) => {
     // TODO ver si es admin o usuario comun y redireccionar acorde
 }))
 
-/*  el metodo permite que un usuari
+/*  el metodo permite que un usuario
     realize la modificacion
     de su mail o contraseña
 */
 .post('/modificar-datos', async (req, res) => {
-    const { mail1, mail2, contraseña1, contraseña2 } = req.body;
-    let user = await User.findOne({ mail: mail1 });
-    if (!user) return res.status(400).send('El mail ingresado no pertenece a ningun usuario en el sistema');
-    if (!await bcrypt.compare(contraseña1, user.contraseña)) return res.status(400).send('La contraseña ingresada no es correcta');
-    if (mail2 === "") mail2 = mail1;
-    if (contraseña2 === "") contraseña2 = contraseña1;
+    let { mailNuevo, contraseña1, contraseña2 } = req.body;
+    let mailActual = req.user.mail;
+    let user = await User.findOne({ mail: mailActual });
+    if (!await bcrypt.compare(contraseña1, user.contraseña)) return res.status(400).json('La contraseña ingresada no es correcta');
     try {
-      const salt = await bcrypt.genSalt(10);
-      user.contraseña = bcrypt.hash(salt);
-      await User.updateOne({ mail: mail1 }, { $set: {
-        mail: mail2,
+      if (mailNuevo === "") mailNuevo = mailActual;
+      if (contraseña2 !== "") {
+        contraseña2 = await encriptarContraseña(contraseña2);
+      }
+      else {
+        contraseña2 = contraseña1;
+        contraseña2 = await encriptarContraseña(contraseña2);
+      }
+      await User.updateOne({ mail: mailActual }, { $set: {
+        mail: mailNuevo,
         contraseña: contraseña2
       }});
       return res.redirect('/');
