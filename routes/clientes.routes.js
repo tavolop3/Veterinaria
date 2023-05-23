71f91814a34e95339b85ca296da976cf16953e71
tavolop3 <tavo.lop33@gmail.com>
2023-05-22 22:18:36 -0300

    Commit de no me acuerdo para pullear

--- routes/clientes.routes.js
+++ routes/clientes.routes.js
@@ -2,17 +2,6 @@ const express = require('express');
 const router = express.Router();
 const autenticado = require('../middleware/autenticado');
 
-// Para modificar la contraseña en el primer ingreso
-router.post('/modificar-pass',autenticado,async(req,res) => {
-    const user = User.findById(req.user._id);
 
-    if(req.body.contraseña1 != req.body.contraseña2)
-      res.redirect('/usuarios/modificar-pass', { error : 'Las contraseñas deben ser iguales' })
-    
-    user.contraseña = await encriptarContraseña(req.body.contraseña1);
-    user.primerLogin = false;
-
-    await user.save();
-})
 
 module.exports = router;
